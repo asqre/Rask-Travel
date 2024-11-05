1d829e9651186c43b5410adb46ba7925b304b8e1
asqre <amitanand.asqre@gmail.com>
2024-11-06 03:20:12 +0530

    fixed contact

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,12 +1,43 @@
-import React from 'react'
-import Layout from '../layout/Layout'
+import React from "react";
+import Layout from "../layout/Layout";
 
 const Contact = () => {
   return (
     <Layout>
-      Contact
-    </Layout>
-  )
-}
+      <div className="max-w-[537px] pt-[60px] m-auto mb-[180px]">
+        <div
+          className="w-full h-full relative"
+          style={{
+            border: "1px solid #eee",
+          }}
+        >
+          <div
+            className="absolute top-[5px] left-[4px] w-full h-full"
+            style={{
+              border: "1px solid #eee",
+              pointerEvents: "none",
+            }}
+          />
 
-export default Contact
+          <a
+            href="https://www.google.com/maps/place/9+Appold+St,+London+EC2A+2AP,+UK/"
+            target="_blank"
+            rel="noreferrer"
+            className="px-[80px] py-[50px] flex flex-col gap-2 text-center"
+          >
+            <h1>RASK Travel</h1>
+            <h6>9 Appold St</h6>
+            <h6>8th Floor</h6>
+            <h6>London EC2A 2AP</h6>
+            <h6>United Kingdom</h6>
+            <a href="mailto:info@rasktravel.com">
+              <h6 className="font-bold underline">info@rasktravel.com</h6>
+            </a>
+          </a>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default Contact;
