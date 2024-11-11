0ce625772da413423995dc0ca731a75829490723
asqre <amitanand.asqre@gmail.com>
2024-11-11 10:31:35 +0530

    added mobile logo

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,11 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../assets/logo.png";
+import mobileLogo from "../assets/mobile_logo.png";
+import useScreenWidth from "../hooks/useScreenWidth";
 
 const Logo = () => {
+  const screenWidth = useScreenWidth();
+  const isMobile = screenWidth < 640;
+
   return (
     <Link to={"/"}>
-      <img src={logo} alt="logo" width="140" />
+      <img src={isMobile ? mobileLogo : logo} alt="logo" width="140" />
     </Link>
   );
 };
